692048d6d2be5d629ed541c63b97f25dcc290145
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-08-23 15:09:44 +0000

    Refactor payment page to use Paystack test key for testing purposes

--- pages/payment.js
+++ pages/payment.js
@@ -1,8 +1,8 @@
 function payWithPaystack() {
     var handler = PaystackPop.setup({
-        key: '', // Replace with your Paystack public key
+        key: 'pk_test_36baf0a3f26aa4acc1993918bb494422b073b581', //This is jusr a tesk key.
         email: document.getElementById('email').value,
-        amount: document.getElementById('amount').value * 100, // Amount in kobo
+        amount: document.getElementById('amount').value * 100, // Amount in cedis
         currency: 'GHS',
         callback: function(response) {
             alert('Payment complete! Reference: ' + response.reference);
