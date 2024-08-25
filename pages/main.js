c3ee7e2ea0144f44a94dcca6afeb2d5d89a5a004
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2024-08-25 08:58:43 +0000

    Refactor CSS styles for improved responsiveness and user experience

---
+++ pages/main.js
@@ -0,0 +1,23 @@
+// script.js
+
+const countdownDate = new Date("Aug 30, 2024 15:00:00").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
